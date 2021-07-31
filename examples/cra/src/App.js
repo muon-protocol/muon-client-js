1f1f84b5eeb9406ab75b7e1e457bd5a99639aff0
Zahra Namazi <namazi.q@gmail.com>
2021-07-31 13:33:37 +0430

    use muon in react example

--- examples/cra/src/App.js
+++ examples/cra/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// import { GetData } from 'muon'
+import Muon from 'muon'
 
 function App() {
+  const [result, setResult] = React.useState([])
+  const [resultCustomApp, setResultCustomAPP] = React.useState([])
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const muon = new Muon('https://node1.muon.net/v1/', 3)
+
+      // call method custom app
+
+      const resultCustomApp = await muon
+        .app('eth')
+        .method('addBridgeToken', {
+          mainTokenAddress: '0x4Ef4E0b448AC75b7285c334e215d384E7227A2E6',
+          mainNetwork: 'bsctest',
+          targetNetwork: 'rinkeby'
+        })
+        .call()
+
+      setResultCustomAPP(JSON.stringify(resultCustomApp, undefined, 2))
+
+      // call method plugin eth
+
+      const result = await muon.apps.eth.addBridgeToken(
+        '0x4Ef4E0b448AC75b7285c334e215d384E7227A2E6',
+        'bsctest',
+        'rinkeby'
+      )
+      setResult(JSON.stringify(result, undefined, 2))
+    }
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Muon Response In React :</h1>
+      <h2>Use muon custom app:</h2>
+      <pre id="json">{resultCustomApp}</pre>
+      <h2>Use muon plugin:</h2>
+      <pre id="json">{result}</pre>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
